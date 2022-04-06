a5b9720733b0cc24ba85e04e805a4920ee11119a
Umme Salma <salmarifa22@gmail.com>
2022-04-06 17:17:52 +0530

    sigh in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KycComponent } from './kyc/kyc.component';
+import { KycStepperComponent } from './kyc-stepper/kyc-stepper.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    KycComponent,
+    KycStepperComponent,
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/kyc-stepper/kyc-stepper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kyc-stepper',
+  templateUrl: './kyc-stepper.component.html',
+  styleUrls: ['./kyc-stepper.component.css']
+})
+export class KycStepperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
